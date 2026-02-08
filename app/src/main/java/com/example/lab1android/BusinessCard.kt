46935451f4438b01eee2209aec39371da4ff25d3
Alexander Mikryukov <alex.mikr.06.03@gmail.com>
2026-02-08 21:30:55 +0400

    Отделение Layout от main, добавление адаптации поворота экрана

---
+++ app/src/main/java/com/example/lab1android/BusinessCard.kt
@@ -0,0 +1,145 @@
+package com.example.lab1android
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Email
+import androidx.compose.material.icons.filled.Phone
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.ImageBitmap
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.imageResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.em
+import com.example.lab1android.ui.theme.Lab1AndroidTheme
+
+@Composable
+fun BusinessCardLayout ()
+{
+    val context = LocalContext.current
+    val isLandscape = context.resources.configuration.orientation == 2
+
+    if(isLandscape)
+        LandscapeLayout()
+    else
+        PortraitLayout()
+}
+
+@Composable
+fun PortraitLayout()
+{
+    Column(
+        modifier = Modifier
+            .fillMaxSize(),
+        verticalArrangement     = Arrangement.SpaceEvenly,
+        horizontalAlignment     = Alignment.CenterHorizontally) {
+        PhotoName()
+        Contacts()
+    }
+}
+
+@Composable
+fun LandscapeLayout()
+{
+    Row(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(24.dp),
+        horizontalArrangement   = Arrangement.SpaceEvenly,
+        verticalAlignment       = Alignment.CenterVertically) {
+        PhotoName()
+        Contacts()
+    }
+}
+
+@Composable
+fun PhotoName() {
+    Column(
+        verticalArrangement     = Arrangement.Center,
+        horizontalAlignment     = Alignment.CenterHorizontally) {
+        Image(
+            modifier = Modifier
+                .size(200.dp)
+                .clip(CircleShape)
+                .background(Color.LightGray),
+            bitmap = ImageBitmap.imageResource(R.drawable.photo),
+            contentDescription = stringResource(R.string.picture_description),
+            contentScale = ContentScale.Crop,
+            alignment = Alignment.Center
+        )
+        Spacer(modifier = Modifier.height(20.dp))
+        Text(
+            text = stringResource(R.string.name),
+            color = MaterialTheme.colorScheme.primary,
+            fontSize = 7.em
+        )
+        Text(
+            text = stringResource(R.string.post),
+            color = MaterialTheme.colorScheme.secondary,
+            fontSize = 4.em
+        )
+    }
+}
+
+@Composable
+fun Contacts ()
+{
+    Column(
+        verticalArrangement     = Arrangement.Center,
+        horizontalAlignment     = Alignment.CenterHorizontally
+    )   {
+        Row(modifier = Modifier.align(Alignment.Start), Arrangement.SpaceBetween) {
+            Icon(
+                Icons.Filled.Phone,
+                contentDescription = stringResource(R.string.phone_icon_description),
+                tint = MaterialTheme.colorScheme.onBackground
+            )
+            Spacer(Modifier.width(5.dp))
+            Text(
+                text = stringResource(R.string.phone) + ": " + stringResource(R.string.phone_number),
+                color = MaterialTheme.colorScheme.onBackground
+            )
+        }
+        Row(modifier = Modifier.align(Alignment.Start), Arrangement.SpaceBetween) {
+            Icon(
+                Icons.Filled.Email,
+                contentDescription = stringResource(R.string.email_icon_description),
+                tint = MaterialTheme.colorScheme.onBackground
+            )
+            Spacer(Modifier.width(5.dp))
+            Text(
+                text = stringResource(R.string.email) + ": " + stringResource(R.string.email_adress),
+                color = MaterialTheme.colorScheme.onBackground
+            )
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Preview(showBackground = true, widthDp = 1000, heightDp = 600)
+@Composable
+fun PreviewBusinessCard() {
+    Lab1AndroidTheme {
+        BusinessCardLayout()
+    }
+}
